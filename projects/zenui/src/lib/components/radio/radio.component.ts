4dd9c1e85ac6741f5af88152d73a2498c41c5a49
sandesh-angularminds <sandesh@angularminds.in>
2025-03-21 19:00:05 +0530

    feature/radio input

--- projects/zenui/src/lib/components/radio/radio.component.ts
+++ projects/zenui/src/lib/components/radio/radio.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'zen-radio',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './radio.component.html',
-  styleUrl: './radio.component.scss'
+  styleUrl: './radio.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true,
+    },
+  ],
 })
-export class RadioComponent {
+export class RadioComponent implements ControlValueAccessor {
+  @Input() name: string = '';
+  @Input() value: string = '';
+  @Input () id: string = "";
+  @Input() label: string = '';
+  @Input() checked: boolean = false;
+  @Input() disabled: boolean = false;
 
+  @Output() change = new EventEmitter<string>();
+
+  onRadioChange(event:any) {
+    // event?.stopPropagation();
+    if (!this.disabled && event) {
+      this.change.emit(this.value);
+    }
+  }
+
+
+  public onChange = (value: boolean) => {};
+  public onTouched = () => {};
+  writeValue(value: boolean): void {
+    console.log('radio value', value);
+    this.checked = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
 }
