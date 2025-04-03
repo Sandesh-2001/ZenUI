392e441522f07e566f6ce470fcc2e56dec6cb8cd
Ajay <ajay@angularminds.in>
2025-04-03 15:15:14 +0530

    Completed table component

--- projects/zenplay/src/app/pages/ui-comp/components/table/table.component.ts
+++ projects/zenplay/src/app/pages/ui-comp/components/table/table.component.ts
@@ -1,12 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ZenUiModule } from '../../../../../../../zenui/src/public-api';
+import { HttpClient } from '@angular/common/http';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-table',
-  imports: [ZenUiModule],
+  imports: [ZenUiModule, NgClass],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrl: './table.component.scss',
 })
 export class TableComponent {
+  httpClient = inject(HttpClient);
 
+  tableData: any = [];
+  stringComparator = (a: string, b: string) => a.localeCompare(b);
+  sorts:{prop : string, dir : "asc" | "desc"}[] = [{prop : "title", dir : "asc"}, {prop : "returnPolicy", dir : "desc"}]
+
+  columns: {
+    prop: string;
+    title: string;
+    sortable?: boolean;
+    comparator?: (a: any, b: any) => number;
+    frozenLeft?:boolean,
+    width?:number
+  }[] = [
+    { prop: 'title', title: 'Title', sortable: true, comparator: this.stringComparator, frozenLeft: true},
+    { prop: 'category', title: ' Category', sortable: false },
+    { prop: 'price', title: 'Price', sortable: false },
+    { prop: 'rating', title: 'Rating', sortable: false },
+    { prop: 'returnPolicy', title: 'Return Policy' },
+    { prop: 'availabilityStatus', title: 'Availability Status' },
+    { prop: "description", title : "Description", width: 800 }
+  ];
+  isLoading: boolean = true;
+  rowHeight = 40;
+  checkBoxFixedPosition:any = 'left'
+
+  page: any = {
+    pageNumber: 0,
+    size: 10,
+    totalElements: 30,
+    totalPages: 1,
+  };
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.isLoading = true;
+    this.httpClient
+      .get(
+        `https://dummyjson.com/products?page=${
+          this.page.pageNumber + 1
+        }&limit=${this.page.size}`
+      )
+      .subscribe({
+        next: (data: any) => {
+          this.tableData = data.products;
+          this.isLoading = false;
+          const totalProducts = data.products.length;
+          this.page.totalElements = 30;
+          this.page.totalPages = Math.ceil(totalProducts / this.page.size);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
+
+  onPageChange(pageNumber: number) {
+    console.log(pageNumber);
+
+    this.page.pageNumber = pageNumber;
+    this.fetchData();
+  }
+
+  onSelectionChange(evt: any) {
+    console.log(evt);
+  }
+
+  handleSortChange(sortingCriteria: { column: string; direction: string }[]) {
+    console.log('Received sorting criteria:', sortingCriteria);
+    // Apply sorting logic if needed
+  }
+  
 }
--- projects/zenui/src/lib/components/table/table.component.ts
+++ projects/zenui/src/lib/components/table/table.component.ts
@@ -1,64 +1,85 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { ColumnMode, SelectionType } from './public-types/public-types';
+import { DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'zen-table',
-  imports: [NgxDatatableModule],
+  imports: [NgxDatatableModule, DragDropModule, NgClass],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
 export class TableComponent {
-  @Input() rows:any = [
-    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
-    { name: 'Dany', gender: 'Male', company: 'KFC' },
-    { name: 'Molly', gender: 'Female', company: 'Burger King' },
-    { name: 'Sarah', gender: 'Female', company: 'McDonalds' },
-    { name: 'Chris', gender: 'Male', company: 'Subway' },
-    { name: 'John', gender: 'Male', company: 'Pizza Hut' },
-    { name: 'Linda', gender: 'Female', company: 'Starbucks' },
-    { name: 'Michael', gender: 'Male', company: 'Dunkin Donuts' }
-  ];
-
-  tempRows:any = [];
-
-  @Input() columns: { prop: string }[] = [{ prop: 'name' }, { prop: 'gender' }, { prop: 'company' }];
+  @Input() rows: any = [];
+  @Input() columns: {
+    prop: string;
+    title: string;
+    sortable?: boolean;
+    comparator?: (a: any, b: any) => number;
+    frozenLeft?: boolean;
+    width?: number;
+  }[] = [];
   @Input() isLoading: boolean = true;
+  @Input() reorderable: boolean = false;
+  @Input() swapColumns: boolean = false;
+  @Input() sorts: { prop: string; dir: 'asc' | 'desc' }[] = [
+    { prop: 'name', dir: 'desc' },
+  ];
+  @Input() rowHeight: any = 'auto';
+  @Input() checkBoxFixedPosition: 'left' | 'right' | 'none' = 'none';
+  @Input() rowTemplate!:TemplateRef<any>;
 
   @Input() page: {
-    pageNumber: number,
-    size: number,
-    totalElements: number,
-    totalPages: number
+    pageNumber: number;
+    size: number;
+    totalElements: number;
+    totalPages: number;
   } = {
     pageNumber: 0,
-    size: 2,
-    totalElements: 8,
-    totalPages: 4
+    size: 10,
+    totalElements: 10,
+    totalPages: 1,
   };
 
   @Output() onRowSelectionChange: EventEmitter<any> = new EventEmitter();
-  @Output() onPageChange:EventEmitter<number> = new EventEmitter();
+  @Output() onPageChange: EventEmitter<number> = new EventEmitter();
+  @Output() onSortChange: EventEmitter<any> = new EventEmitter();
 
   ColumnMode = ColumnMode;
-  SelectionType:any = SelectionType;
+  SelectionType: any = SelectionType;
 
   selectedRows: any[] = [];
 
-  onSelect({ selected }:any) {
+  ngOnInit(){}
+
+  onSelect({ selected }: any) {
     this.selectedRows.splice(0, this.selectedRows.length);
     this.selectedRows.push(...selected);
     this.onRowSelectionChange.emit(this.selectedRows);
   }
 
-  setPage(pageInfo:any) {
-    this.page.pageNumber = pageInfo.offset;
-    this.isLoading = true;
-    setTimeout(() => {
-      this.tempRows = this.rows.slice(this.page.pageNumber, (this.page.pageNumber * this.page.size) + this.page.size);
-      this.isLoading = false;
-    }, 4000)
-    this.onPageChange.emit(pageInfo);
+  setPage(pageInfo: any) {
+    if (pageInfo.offset !== this.page.pageNumber) {
+      this.page.pageNumber = pageInfo.offset;
+      this.onPageChange.emit(pageInfo.offset);
+    }
   }
 
+  onSort(event: any) {
+    const sortingCriteria = event.sorts.map((sort: any) => ({
+      column: sort.prop,
+      direction: sort.dir,
+    }));
+
+    console.log('Sorting Criteria:', sortingCriteria);
+
+    // Emit sorting changes
+    this.onSortChange.emit(sortingCriteria);
+  }
+
+  drop(event: any) {
+    moveItemInArray(this.rows, event.previousIndex, event.currentIndex);
+    this.rows = [...this.rows];
+  }
 }
